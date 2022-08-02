183e2644003d12e25ec459c21ecfe3285e4e91df
SuperCuteXiaoSi <1531733886@qq.com>
2022-08-03 00:14:16 +0800

    fixbug: 修复stylelint 校验问题

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: ['stylelint-scss', 'stylelint-order'],
-  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
   customSyntax: 'postcss-html',
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
   rules: {
     indentation: 2, // 缩进2个空格
     'at-rule-no-unknown': [
@@ -18,11 +18,38 @@ module.exports = {
         ignorePseudoClasses: ['export'],
       },
     ],
+    'unit-no-unknown': [
+      true,
+      {
+        ignoreUnits: ['rpx'],
+      },
+    ],
     // 关闭禁用位置属性，这个对scss导出变量有问题
     'property-no-unknown': null,
     // https://github.com/kristerkari/stylelint-scss scsslint规范配置
     'scss/comment-no-loud': true, // 禁止使用/*注释
     'scss/comment-no-empty': true, // 禁止空注释
   },
+  overrides: [
+    {
+      files: ['*.vue', '**/*.vue', '*.html', '**/*.html'],
+      extends: ['stylelint-config-recommended'],
+      rules: {
+        'keyframes-name-pattern': null,
+        'selector-pseudo-class-no-unknown': [
+          true,
+          {
+            ignorePseudoClasses: ['deep', 'global'],
+          },
+        ],
+        'selector-pseudo-element-no-unknown': [
+          true,
+          {
+            ignorePseudoElements: ['v-deep', 'v-global', 'v-slotted'],
+          },
+        ],
+      },
+    },
+  ],
   ignoreFiles: ['**/*.js', '**/*.ts'],
 };
